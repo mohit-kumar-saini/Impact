a2a6a20eca75dd94daf0defa239c36b0f8c36862
mohit-kumar-saini <mohitsaini2622132@gmail.com>
2025-05-28 22:56:01 +0530

    update of 28th  may

--- 28_MAY/OOPS_Hybrid_Inheritance.java
+++ 28_MAY/OOPS_Hybrid_Inheritance.java
@@ -1,32 +1,41 @@
-interface A {
+interface A 
+{
     void methodA();
 }
-interface B extends A {
+interface B extends A 
+{
     void methodB();
 }
-interface C {
+interface C 
+{
     void methodC();
 }
-class D implements B, C {
-    public void methodA() {
+class D implements B, C 
+{
+    public void methodA() 
+    {
         System.out.println("Method A from Interface A");
     }
 
-    public void methodB() {
+    public void methodB() 
+    {
         System.out.println("Method B from Interface B");
     }
 
-    public void methodC() {
+    public void methodC() 
+    {
         System.out.println("Method C from Interface C");
     }
-
-    void methodD() {
+    void methodD()
+    {
         System.out.println("Method D from Class D");
     }
 }
 
-public class OOPS_Hybrid_Inheritance {
-    public static void main(String[] args) {
+public class OOPS_Hybrid_Inheritance 
+{
+    public static void main(String[] args) 
+    {
         D obj = new D();
         obj.methodA();  
         obj.methodB();  
